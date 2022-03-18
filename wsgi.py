20b417efb0b247778f01a2f154e950519576b38d
Stefane Fermigier <sf@abilian.com>
2022-03-18 16:29:30 +0100

    chore: deps.

--- wsgi.py
+++ wsgi.py
@@ -5,10 +5,10 @@ import newrelic.agent
 
 from labster.app import create_app
 
-newrelic_config = Path(__file__).parent / "newrelic.ini"
-assert newrelic_config.exists()
 
-newrelic.agent.initialize(newrelic_config, environment="production")
+newrelic_config = Path(__file__).parent / "newrelic.ini"
+if newrelic_config.exists():
+    newrelic.agent.initialize(newrelic_config, environment="production")
 
 app = create_app()
 app = newrelic.agent.WSGIApplicationWrapper(app)
