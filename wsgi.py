0a5bd69e6e5b9e5283a77ff162053ce25c321de3
Stefane Fermigier <sf@abilian.com>
2022-03-18 18:46:15 +0100

    fix: don't call newrelic unless we really mean it.

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,17 @@
 """Create an application instance."""
 from pathlib import Path
 
-import newrelic.agent
 
 from labster.app import create_app
+
+app = create_app()
 
 
 newrelic_config = Path(__file__).parent / "newrelic.ini"
 if newrelic_config.exists():
+    import newrelic.agent
     newrelic.agent.initialize(newrelic_config, environment="production")
 
-app = create_app()
-app = newrelic.agent.WSGIApplicationWrapper(app)
+
+    app = newrelic.agent.WSGIApplicationWrapper(app)
+
